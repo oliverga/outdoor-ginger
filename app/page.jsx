1c9f5e694d431c2a3758eee41e588691eb3c13e0
Oliver G.A. <hi@oliverga.com>
2024-05-02 10:11:10 +0200

    added newsletter banner

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,9 @@
+import NewsletterBanner from "@/components/NewsletterBanner";
+
 export default function Home() {
-  return <main></main>;
+  return (
+    <main>
+      <NewsletterBanner />
+    </main>
+  );
 }
--- components/NewsletterBanner.jsx
+++ components/NewsletterBanner.jsx
@@ -1,5 +1,37 @@
+import { Button } from "./ui/button";
+import { Input } from "./ui/input";
+
 function NewsletterBanner() {
-  return <div className="">Enter</div>;
+  return (
+    <section className="h-[50vh] bg-ogPrimary flex flex-col items-center justify-evenly text-center px-4">
+      <div className=" space-y-3">
+        <h1 className="text-6xl md:text-8xl font-medium text-ogPrimary-lightest">
+          Campfire Chronicles
+        </h1>
+        <h2 className=" font-display text-ogPrimary-lighter text-xl font-medium">
+          The Outdoor Ginger Newsletter
+        </h2>
+      </div>
+      <p className="text-ogPrimary-lighter max-w-lg text-lg">
+        Subscribe to my newsletter to get interesting stories, field reports,
+        and other goodies straight to your inbox.
+      </p>
+      <form action="" className="flex flex-col md:flex-row gap-4 items-center">
+        <Input
+          type="email"
+          placeholder="Your email address"
+          className="bg-ogPrimary-light placeholder:text-ogPrimary-lightest text-ogPrimary-lightest border-ogPrimary-lighter"
+          required
+        />
+        <Button
+          size="lg"
+          className="bg-ogPrimary-dark text-ogPrimary-lightest hover:bg-ogPrimary-darker"
+        >
+          Subscribe
+        </Button>
+      </form>
+    </section>
+  );
 }
 
 export default NewsletterBanner;
