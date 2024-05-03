824a85a55eed84089319f1b47508301611b23ce1
Oliver G.A. <hi@oliverga.com>
2024-05-03 16:20:01 +0200

    add tailwind typography

--- tailwind.config.js
+++ tailwind.config.js
@@ -134,5 +134,5 @@ module.exports = {
       "accordion-up": "accordion-up 0.2s ease-out",
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [require("tailwindcss-animate"), require('@tailwindcss/typography')],
 }
