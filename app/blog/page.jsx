00c9a602e72b5a4312b5a8ecfb5766344468ea8b
Oliver G.A. <hi@oliverga.com>
2024-05-03 16:35:21 +0200

    cms fetch test - works!

--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -1,3 +1,43 @@
+import { client } from "@/lib/sanity/client";
+import { PortableText } from "next-sanity";
+import { format } from "date-fns";
+
+const postQuery =
+  '*[_type == "post"]{title, slug, author, publishedAt, mainImage, content}';
+
+const posts = await client.fetch(postQuery, {
+  next: {
+    revalidate: process.env.NODE_ENV === "development" ? 30 : 3600,
+  },
+});
+
+const authorQuery = '*[_type == "author"]{_id, name, image, instagram}';
+
+const authors = await client.fetch(authorQuery);
+
 export default function Page() {
-  return <main></main>;
+  return (
+    <main>
+      {posts.map((post) => {
+        if (!post.author) {
+          console.error("Post author is null:", post);
+          return null; // or handle this case differently as needed
+        }
+        const postAuthor = authors.find(
+          (author) => author._id === post.author._ref,
+        );
+        return (
+          <article
+            key={post.slug.current}
+            className="prose md:prose-xl mx-auto mt-20 mb-48 px-6 border-b pb-32"
+          >
+            <h2>{post.title}</h2>
+            <p>By {postAuthor.name}</p>
+            <p>{format(new Date(post.publishedAt), "PPP")}</p>
+            <PortableText value={post.content} />
+          </article>
+        );
+      })}
+    </main>
+  );
 }
