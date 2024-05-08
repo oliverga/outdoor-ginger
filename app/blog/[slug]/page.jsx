e49407d1f21c87fcf290963ead11534181f23418
Oliver G.A. <hi@oliverga.com>
2024-05-08 12:03:58 +0200

    blog setup

--- app/blog/[slug]/page.jsx
+++ app/blog/[slug]/page.jsx
@@ -1,3 +1,71 @@
-export default function Page({ params }) {
-  return <main></main>;
+import { client, urlFor } from "@/lib/sanity/client";
+import { PortableText } from "next-sanity";
+import Image from "next/image";
+
+export async function generateStaticParams() {
+  const slugQuery = '*[_type == "post"]{slug}';
+  const posts = await client.fetch(slugQuery, {
+    next: {
+      revalidate: process.env.NODE_ENV === "development" ? 30 : 3600,
+    },
+  });
+
+  return posts.map((post) => ({
+    params: { slug: post.slug.current },
+  }));
+}
+
+export default async function Page({ params }) {
+  const postQuery =
+    '*[_type == "post" && slug.current == $slug]{slug, title, author, publishedAt, mainImage, content}';
+
+  const post = await client.fetch(postQuery, {
+    slug: params.slug,
+  });
+
+  const authorQuery = '*[_type == "author"]{_id, name, mainImage, instagram}';
+  const authors = await client.fetch(authorQuery);
+
+  const postAuthor = authors.find(
+    (author) => author._id === post[0].author._ref,
+  );
+
+  console.log("Post:", post);
+
+  return (
+    <main>
+      <div className="h-[50vh] overflow-hidden ">
+        <Image
+          src={urlFor(post[0].mainImage).url()}
+          alt=""
+          width={1920}
+          height={1000}
+          className="object-cover object-center w-full h-full"
+        />
+      </div>
+      <div className=" pt-[5rem] rounded-t-[3rem] -translate-y-[3rem] bg-ogBG-base">
+        <article className="prose md:prose-xl mx-auto p-8">
+          <h1 className="text-3xl md:text-5xl">{post[0].title}</h1>
+          <p>
+            By{" "}
+            <a
+              href={`https://instagram.com/${postAuthor.instagram}`}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="underline"
+            >
+              {postAuthor.name}
+            </a>{" "}
+            on{" "}
+            {new Date(post[0].publishedAt).toLocaleDateString("en-US", {
+              year: "numeric",
+              month: "long",
+              day: "numeric",
+            })}
+          </p>
+          <PortableText value={post[0].content} />
+        </article>
+      </div>
+    </main>
+  );
 }
--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -1,6 +1,7 @@
-import { client } from "@/lib/sanity/client";
-import { PortableText } from "next-sanity";
+import { client, urlFor } from "@/lib/sanity/client";
 import { format } from "date-fns";
+import Image from "next/image";
+import Link from "next/link";
 
 const postQuery =
   '*[_type == "post"]{title, slug, author, publishedAt, mainImage, content}';
@@ -11,37 +12,50 @@ const posts = await client.fetch(postQuery, {
   },
 });
 
-const authorQuery = '*[_type == "author"]{_id, name, image, instagram}';
+console.log(posts);
+
+const authorQuery = '*[_type == "author"]{_id, name, mainImage, instagram}';
 
 const authors = await client.fetch(authorQuery);
 
 export default function Page() {
   return (
-    <main className="pt-32">
-      {posts.map((post) => {
-        if (!post.author) {
-          console.error("Post author is null:", post);
-          return null; // or handle this case differently as needed
-        }
-        const postAuthor = authors.find(
-          (author) => author._id === post.author._ref,
-        );
-        return (
-          <article
-            key={post.slug.current}
-            className="prose md:prose-lg prose-neutral mx-auto mt-20 mb-48 px-6 border-b pb-32"
-          >
-            <h2>{post.title}</h2>
-
-            <p>By {postAuthor.name}</p>
-            <p className="text-base">
-              {format(new Date(post.publishedAt), "PPP")}
-            </p>
-
-            <PortableText value={post.content} />
-          </article>
-        );
-      })}
+    <main className="pt-64 pb-32 max-w-7xl mx-auto">
+      <div className="grid grid-flow-col grid-cols-3 gap-2">
+        {posts.map((post) => {
+          if (!post.author) {
+            console.error("Post author is null:", post);
+            return null; // or handle this case differently as needed
+          }
+          const postAuthor = authors.find(
+            (author) => author._id === post.author._ref,
+          );
+          return (
+            <article
+              key={post.slug.current}
+              className="mx-auto border rounded-xl overflow-hidden"
+            >
+              <Link href={`/blog/${post.slug.current}`}>
+                {post.mainImage && (
+                  <Image
+                    src={urlFor(post.mainImage).url()}
+                    alt=""
+                    width={800}
+                    height={400}
+                    className="aspect-video"
+                  />
+                )}
+                <div className="m-4 space-y-2">
+                  <h2 className=" font-semibold text-lg">{post.title}</h2>
+                  <p className="text-sm text-ogLabel-muted">
+                    {format(new Date(post.publishedAt), "PPP")}
+                  </p>
+                </div>
+              </Link>
+            </article>
+          );
+        })}
+      </div>
     </main>
   );
 }
