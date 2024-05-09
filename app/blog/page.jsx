46b49377e394d08ca71ebe5c03ddca3ceb15cfd0
Oliver G.A. <hi@oliverga.com>
2024-05-09 10:40:49 +0200

    blog shiz

--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -21,7 +21,7 @@ const authors = await client.fetch(authorQuery);
 export default function Page() {
   return (
     <main className="pt-64 pb-32 max-w-4xl mx-auto ">
-      <div className="grid grid-cols-3 gap-6 ">
+      <div className="flex flex-col md:grid grid-cols-3 gap-6">
         <div className=" col-span-1 flex flex-col gap-6">
           {posts.slice(0, Math.ceil(posts.length / 2)).map((post) => {
             return <Article post={post} key={post.slug.current} />;
@@ -38,29 +38,30 @@ export default function Page() {
 }
 
 function Article({ post }) {
-  const postAuthor = authors.find((author) => author._id === post.author._ref);
   return (
-    <article
-      key={post.slug.current}
-      className="mx-auto border rounded-xl overflow-hidden w-full"
-    >
-      <Link href={`/blog/${post.slug.current}`}>
-        {post.mainImage && (
-          <Image
-            src={urlFor(post.mainImage).url()}
-            alt=""
-            width={800}
-            height={400}
-            className="aspect-video"
-          />
-        )}
-        <div className="m-4 space-y-2">
-          <h2 className=" font-semibold text-lg">{post.title}</h2>
-          <p className="text-sm text-ogLabel-muted">
-            {format(new Date(post.publishedAt), "PPP")}
-          </p>
-        </div>
-      </Link>
-    </article>
+    <div className="@container">
+      <article
+        key={post.slug.current}
+        className="mx-auto border rounded-xl overflow-hidden w-full"
+      >
+        <Link href={`/blog/${post.slug.current}`}>
+          {post.mainImage && (
+            <Image
+              src={urlFor(post.mainImage).url()}
+              alt=""
+              width={800}
+              height={400}
+              className="aspect-video @xl:aspect-[16/7] object-cover object-center w-full  "
+            />
+          )}
+          <div className="m-4 space-y-2">
+            <h2 className=" font-medium text-xl">{post.title}</h2>
+            <p className="text-xs text-ogLabel-faint">
+              {format(new Date(post.publishedAt), "PPP")}
+            </p>
+          </div>
+        </Link>
+      </article>
+    </div>
   );
 }
