846f6e73db09271f2b2ed2d9f14fdcf8380d718a
Anders Lindberg <anderslindberg999@gmail.com>
2024-05-09 12:03:46 +0200

    update

--- app/membership/page.jsx
+++ app/membership/page.jsx
@@ -1,11 +1,32 @@
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion";
 import SupportBanner from "@/components/SupportBanner";
 import Hero from "@/components/generel/Hero";
-
+import Image from "next/image";
 export default function Page() {
   return (
     <main>
       <Hero title="Membership" imageSrc="/membershiphero.webp" height="60vh"></Hero>
-      <div className="pt-64 pb-32 max-w-7xl mx-auto"></div>
+      <div className="py-32 max-w-5xl mx-auto">
+        <div className="flex justify-between">
+          <Accordion type="single" collapsible className="w-1/2">
+            <AccordionItem value="item-1">
+              <AccordionTrigger>What is a membership?</AccordionTrigger>
+              <AccordionContent className="text-ogLabel-muted text-sm">Yes. It adheres to the WAI-ARIA design pattern.</AccordionContent>
+            </AccordionItem>
+            <AccordionItem value="item-2">
+              <AccordionTrigger>What's in it for me?</AccordionTrigger>
+              <AccordionContent className="text-ogLabel-muted text-sm">Yes. It comes with default styles that matches the other components&apos; aesthetic.</AccordionContent>
+            </AccordionItem>
+            <AccordionItem value="item-3">
+              <AccordionTrigger>What does it cost?</AccordionTrigger>
+              <AccordionContent className="text-ogLabel-muted text-sm">Yes. It&apos;s animated by default, but you can disable it if you prefer.</AccordionContent>
+            </AccordionItem>
+          </Accordion>
+          <div className="w-1/2 flex justify-end">
+            <Image src="/membershipvideo.png" width={400} height={200} alt="Your Description" className="" />
+          </div>
+        </div>
+      </div>
       <SupportBanner />
     </main>
   );
