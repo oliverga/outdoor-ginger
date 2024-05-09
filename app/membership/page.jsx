67aba2cb7994a58286c24dffb0c12fac8fabeadb
Anders Lindberg <anderslindberg999@gmail.com>
2024-05-09 10:07:46 +0200

    hero

--- app/membership/page.jsx
+++ app/membership/page.jsx
@@ -4,8 +4,9 @@ import Hero from "@/components/generel/Hero";
 export default function Page() {
   return (
     <main>
-      <Hero title="Membership" height="650px" imageSrc="/membershiphero.webp"></Hero>
-
+      <Hero title="Membership" imageSrc="/membershiphero.webp"></Hero>
+      <div className=" pt-[5rem] rounded-t-[3rem] -translate-y-[3rem] bg-ogBG-base"></div>
+      <div className="pt-64 pb-32 max-w-7xl mx-auto"></div>
       <SupportBanner />
     </main>
   );
--- components/generel/Hero.jsx
+++ components/generel/Hero.jsx
@@ -1,12 +1,11 @@
 import Image from "next/image";
 
-function Hero({ title, height, imageSrc, children }) {
+function Hero({ title, imageSrc, children }) {
   return (
-    <div className="relative pt-32 w-screen" style={{ height: height, position: "relative" }}>
-      <Image src={imageSrc} alt="Mountain View" layout="fill" objectFit="cover" quality={100} className="absolute z-0" />
-      <div className="max-w-7xl mx-auto py-4 relative z-10 flex flex-col justify-between h-full">
-        <div className="flex justify-end pt-16 pr-44"></div>
-        <h1 className="text-4xl md:text-7xl pl-28 pb-12 font-bold text-ogBG-base uppercase z-50">{title}</h1>
+    <div className="h-[100vh] overflow-hidden">
+      <Image src={imageSrc} alt="" width={1920} height={1000} className="object-cover object-center w-full h-full z-30" />
+      <div>
+        <h1 className="text-4xl md:text-7xl pl-28 pb-12 font-bold text-ogBG-base uppercase z-60">{title}</h1>
         {children}
       </div>
     </div>
