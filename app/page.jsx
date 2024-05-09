3dd11f891565898fe8e9ce3c097018c592a3ae2b
jaco266w <jaco266w@stud.kea.dk>
2024-05-09 10:27:40 +0200

    hero på page

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,13 @@
 import NewsletterBanner from "@/components/NewsletterBanner";
 import SupportBanner from "@/components/SupportBanner";
+import Hero from "@/components/generel/Hero";
 
 export default function Home() {
-  return (
-    <main>
-      <SupportBanner />
-      <NewsletterBanner />
-    </main>
-  );
+	return (
+		<main>
+			<Hero></Hero>
+			<SupportBanner />
+			<NewsletterBanner />
+		</main>
+	);
 }
