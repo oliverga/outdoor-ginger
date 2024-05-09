9842256ec03fff1aff4d729c6fbfc0113cea599c
Anders Lindberg <anderslindberg999@gmail.com>
2024-05-09 10:33:46 +0200

    update

--- app/membership/page.jsx
+++ app/membership/page.jsx
@@ -1,14 +1,10 @@
 import SupportBanner from "@/components/SupportBanner";
 import Hero from "@/components/generel/Hero";
 
-import SupportBanner from "@/components/SupportBanner";
-import Hero from "@/components/generel/Hero";
-
 export default function Page() {
   return (
     <main>
       <Hero title="Membership" height="650px" imageSrc="/membershiphero.webp"></Hero>
-
       <SupportBanner />
     </main>
   );
