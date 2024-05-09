f7ec5831e128e92501cb28c86ee51bea83427052
jaco266w <jaco266w@stud.kea.dk>
2024-05-09 11:46:49 +0200

    latestYT setup

--- app/page.jsx
+++ app/page.jsx
@@ -1,19 +1,21 @@
 import NewsletterBanner from "@/components/NewsletterBanner";
 import SupportBanner from "@/components/SupportBanner";
+import LatestYT from "@/components/generel/LatestYT";
 import Hero from "@/components/generel/Hero";
 
 export default function Home() {
-  return (
-    <main>
-      <Hero
-        title="Outdoor Ginger"
-        imageSrc="/forsidehero.webp"
-        height="80vh"
-        bgPos="bottom"
-      ></Hero>
-      <section className="h-screen"></section>
-      <SupportBanner />
-      <NewsletterBanner />
-    </main>
-  );
+	return (
+		<main>
+			<Hero
+				title="Outdoor Ginger"
+				imageSrc="/forsidehero.webp"
+				height="80vh"
+				bgPos="bottom"
+			></Hero>
+			<section className="h-screen"></section>
+			<LatestYT />
+			<SupportBanner />
+			<NewsletterBanner />
+		</main>
+	);
 }
