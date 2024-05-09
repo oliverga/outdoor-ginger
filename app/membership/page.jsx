db913be8b1c03ca3cba807179e22f8de49de659f
Anders Lindberg <anderslindberg999@gmail.com>
2024-05-09 13:12:12 +0200

    Memebrship-update-with hero children

--- app/membership/page.jsx
+++ app/membership/page.jsx
@@ -1,15 +1,37 @@
 import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion";
 import SupportBanner from "@/components/SupportBanner";
+import { Button } from "@/components/ui/button";
 import Hero from "@/components/generel/Hero";
 import Image from "next/image";
+
+import { Card, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
 export default function Page() {
   return (
-    <main>
-      <Hero title="Membership" imageSrc="/membershiphero.webp" height="60vh"></Hero>
+    <main className="relative">
+      <Hero title="Membership" imageSrc="/membershiphero.webp" height="60vh">
+        <div id="right" className="mt-10 w-80">
+          <Card>
+            <CardHeader>
+              <CardTitle className="font-normal">Become a Member</CardTitle>
+              <CardDescription className="font-light">
+                Join our community of explorers <br></br> and get exclusive benefits.
+              </CardDescription>
+            </CardHeader>
+            <CardFooter className="flex justify-between">
+              <p className="text-3xl font-semibold">
+                50 kr <span className="text-sm font-light text-ogLabel-muted ">/ Month </span>
+              </p>
+              <Button size="sm" className=" bg-ogPrimary hover:bg-ogPrimary-dark gap-1">
+                Subscribe
+              </Button>
+            </CardFooter>
+          </Card>
+        </div>
+      </Hero>
       <div className="py-32 max-w-5xl mx-auto">
         <div className="flex justify-between">
           <Accordion type="single" collapsible className="w-1/2">
-            <AccordionItem value="item-1">
+            <AccordionItem value="item-1" defaultOpen={true}>
               <AccordionTrigger>What is a membership?</AccordionTrigger>
               <AccordionContent className="text-ogLabel-muted text-sm">Yes. It adheres to the WAI-ARIA design pattern.</AccordionContent>
             </AccordionItem>
