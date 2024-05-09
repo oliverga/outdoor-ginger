2aa081c4637480a0ffee2037ce937ef561fb5c46
Anders Lindberg <anderslindberg999@gmail.com>
2024-05-09 10:25:44 +0200

    hero with Jydebuen

--- app/membership/page.jsx
+++ app/membership/page.jsx
@@ -5,7 +5,6 @@ export default function Page() {
   return (
     <main>
       <Hero title="Membership" imageSrc="/membershiphero.webp"></Hero>
-      <div className=" pt-[5rem] rounded-t-[3rem] -translate-y-[3rem] bg-ogBG-base"></div>
       <div className="pt-64 pb-32 max-w-7xl mx-auto"></div>
       <SupportBanner />
     </main>
--- components/generel/Hero.jsx
+++ components/generel/Hero.jsx
@@ -2,13 +2,16 @@ import Image from "next/image";
 
 function Hero({ title, imageSrc, children }) {
   return (
-    <div className="h-[100vh] overflow-hidden">
-      <Image src={imageSrc} alt="" width={1920} height={1000} className="object-cover object-center w-full h-full z-30" />
-      <div>
-        <h1 className="text-4xl md:text-7xl pl-28 pb-12 font-bold text-ogBG-base uppercase z-60">{title}</h1>
-        {children}
+    <section>
+      <div className="h-[90vh] overflow-hidden grid grid-cols-2">
+        <Image src={imageSrc} alt="" width={1920} height={1000} className="object-cover object-center w-full h-full z-10 row-start-1 col-start-1 col-span-2" />
+        <div className="place-self-end col-start-1 row-start-1 z-20">
+          <h1 className="text-4xl md:text-7xl font-bold text-ogBG-base uppercase mb-20">{title}</h1>
+          <div className="h-20">{children}</div>
+        </div>
+        <div className="pt-[5rem] rounded-t-[3rem] -translate-y-[3rem] bg-ogBG-base z-30 col-span-2"></div>
       </div>
-    </div>
+    </section>
   );
 }
 
