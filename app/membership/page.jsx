ce609e20fbf8c8f627840fb3c892e8d5b2008a03
Anders Lindberg <anderslindberg999@gmail.com>
2024-05-09 12:03:46 +0200

    update

--- app/membership/page.jsx
+++ app/membership/page.jsx
@@ -1,12 +1,13 @@
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from "@/components/ui/accordion";
 import SupportBanner from "@/components/SupportBanner";
 import Hero from "@/components/generel/Hero";
-
+import Image from "next/image";
 export default function Page() {
   return (
     <main>
-      <Hero title="Membership" height="650px" imageSrc="/membershiphero.webp"></Hero>
-      <div className="pb-32 max-w-5xl mx-auto">
-        <div className="flex flex-row gap-12 justify-between">
+      <Hero title="Membership" imageSrc="/membershiphero.webp" height="60vh"></Hero>
+      <div className="py-32 max-w-5xl mx-auto">
+        <div className="flex justify-between">
           <Accordion type="single" collapsible className="w-1/2">
             <AccordionItem value="item-1">
               <AccordionTrigger>What is a membership?</AccordionTrigger>
@@ -21,7 +22,9 @@ export default function Page() {
               <AccordionContent className="text-ogLabel-muted text-sm">Yes. It&apos;s animated by default, but you can disable it if you prefer.</AccordionContent>
             </AccordionItem>
           </Accordion>
-          <img src="/your-image.jpg" alt="Your Description" className=" justify-self-end" />
+          <div className="w-1/2 flex justify-end">
+            <Image src="/membershipvideo.png" width={400} height={200} alt="Your Description" className="" />
+          </div>
         </div>
       </div>
       <SupportBanner />
