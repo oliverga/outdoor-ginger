358040b1debb6593992a4e67181afcca6298f80d
jaco266w <jaco266w@stud.kea.dk>
2024-05-10 09:31:55 +0200

    logic for api call

--- components/generel/LatestYT.jsx
+++ components/generel/LatestYT.jsx
@@ -8,23 +8,29 @@ export default function LatestYT() {
 	useEffect(() => {
 		const apiKey = "AIzaSyBDx6YWmbbZ-on7sEt3sRtiER6XtZEhATk";
 		const channelId = "UCo7ll072evLTI3hawAW3pMQ"; // Replace with the Channel ID of the desired YouTube channel
-		const maxResults = 3; // Number of latest videos to retrieve
+		const maxResults = 50; // Number of latest videos to retrieve
+		console.log(
+			`https://www.googleapis.com/youtube/v3/search?key=${apiKey}&channelId=${channelId}&part=snippet,id&order=date&maxResults=${maxResults}`
+		);
 
 		fetch(
 			`https://www.googleapis.com/youtube/v3/search?key=${apiKey}&channelId=${channelId}&part=snippet,id&order=date&maxResults=${maxResults}`
 		)
 			.then((response) => response.json())
 			.then((data) => {
-				const videoData = data.items.map((item) => {
-					const videoId = item.id.videoId;
-					const videoTitle = item.snippet.title;
+				const videoData = data.items
+					.filter((item) => item.snippet.description !== "")
+					.slice(0, 3) // Limit the results to 3 videos
+					.map((item) => {
+						const videoId = item.id.videoId;
+						const videoTitle = item.snippet.title;
 
-					return {
-						id: videoId,
-						title: videoTitle,
-						url: `https://www.youtube.com/embed/${videoId}`,
-					};
-				});
+						return {
+							id: videoId,
+							title: videoTitle,
+							url: `https://www.youtube.com/embed/${videoId}`,
+						};
+					});
 
 				setVideos(videoData);
 			})
@@ -38,13 +44,13 @@ export default function LatestYT() {
 			<h2 className="text-orange">Latest YouTube Videos</h2>
 			{videos.map((video) => (
 				<div key={video.id}>
-					<h3>{video.title}</h3>
 					<iframe
 						width="560"
 						height="315"
 						src={video.url}
 						frameBorder="0"
 						allowFullScreen
+						title="YouTube video player"
 					/>
 				</div>
 			))}
