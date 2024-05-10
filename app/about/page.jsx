7ba165d1bb67639132c2e1961222e6a3d5d76db9
Anders Lindberg <anderslindberg999@gmail.com>
2024-05-10 11:50:21 +0200

    page update

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,5 +1,5 @@
 import Hero from "@/components/generel/Hero";
-
+import Image from "next/image";
 export default function Page() {
   return (
     <main className="">
@@ -8,9 +8,19 @@ export default function Page() {
           <p className="text-ogLabel-faint font-extralight md:text-2xl italic">Anything is possible if you dare to be curious</p>
         </div>
       </Hero>
-      <section className="px-6 md:px-0 py-32 max-w-5xl mx-auto">
+      <section className="px-6 md:px-0 pb-32 max-w-5xl mx-auto">
         <div>
-          <h2 className="font-display text-3xl uppercase font-bold">august</h2>
+          <h2 className="font-display text-3xl uppercase font-bold mb-6">august</h2>
+          <p>
+            August, known as the outdoorginger, embarked on his journey with the wind in his hair and the thrill of adventure in his heart. From a young age, he embraced the call of the wild, navigating the trails and conquering the great outdoors. With each expedition, he honed
+            his skills and forged a path of his own, earning accolades and admiration along the way. Now, as a seasoned explorer, August stands ready to inspire and ignite the spirit of adventure in all who dare to follow.
+          </p>
+        </div>
+        <div className="mt-6">
+          <Image src="/aboutMe1.png" alt="" width={400} height={200} className="" />
+        </div>
+        <div>
+          <div></div>
         </div>
       </section>
     </main>
