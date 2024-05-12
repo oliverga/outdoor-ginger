b44a5f663e53a72816cee7d6aa03517e32fcbcd3
jaco266w <jaco266w@stud.kea.dk>
2024-05-12 12:23:34 +0200

    cart better design

--- components/ShoppingCart.jsx
+++ components/ShoppingCart.jsx
@@ -2,18 +2,44 @@
 
 import Item from "./Cart/Item";
 import { motion, AnimatePresence } from "framer-motion";
+import Link from "next/link";
+import { Checkbox } from "./ui/checkbox";
 
 export default function ShoppingCart({ cartOpen }) {
 	return (
-		<div className="overflow-hidden fixed top-0 left-0 z-40 w-full px-2 mt-12">
+		<div className="overflow-hidden fixed top-0 left-0 z-40 w-full px-2 mt-12 drop-shadow-md">
 			<motion.div
 				initial={{ y: "-100vh" }}
 				animate={{ y: cartOpen ? "0vh" : "-100vh" }}
 				transition={{ duration: 0.5 }}
 				className=""
 			>
-				<div className="bg-ogPrimary h-80 pt-16 px-8">
-					<Item />
+				<div className="bg-ogBG-base rounded-b-2xl overflow-hidden h-96 pt-16 flex flex-col justify-between">
+					<div className="px-8">
+						<Item />
+					</div>
+					<div className="bg-neutral-200">
+						<div className="px-8 pt-4 pb-6 flex flex-col gap-1">
+							<div className=" flex justify-between">
+								<p>Subtotal</p>
+								<p>99.95 kr</p>
+							</div>
+							<hr className="bg-neutral-300 h-[3px]" />
+							<div className="flex items-center gap-2">
+								<Checkbox />
+								<p>
+									Accept{" "}
+									<Link href="https://www.google.com/search?client=firefox-b-d&q=terms+and+conditions">
+										Terms
+									</Link>
+									{" & "}
+									<Link href="https://www.google.com/search?client=firefox-b-d&q=terms+and+conditions">
+										Conditions
+									</Link>
+								</p>
+							</div>
+						</div>
+					</div>
 				</div>
 			</motion.div>
 		</div>
