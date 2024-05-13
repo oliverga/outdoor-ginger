5167d8e4120325fe0ee200fcc41bd7fb48957e39
jaco266w <jaco266w@stud.kea.dk>
2024-05-13 09:36:57 +0200

    style: Update AboutBlock component and CSS

--- components/generel/AboutBlock.jsx
+++ components/generel/AboutBlock.jsx
@@ -2,19 +2,26 @@ import Image from "next/image";
 
 export default function AboutBlock() {
 	return (
-		<article>
-			<div className="flex flex-col self-center">
+		<article className="px-8">
+			<div className="flex flex-col items-center">
 				<div className="w-40">
 					<Image
 						src="/waterSitting.png"
 						alt="Hero image"
 						width={1000}
 						height={1000}
-						className="rounded-full object-cover w-full h-full"
+						className="rounded-full object-cover w-full h-full drop-shadow-lg"
 					/>
 				</div>
-				<h2 className="uppercase">Outdoorginger</h2>
+				<h2 className="uppercase text-center font-display font-semibold text-ogLabel-base text-4xl md:text-5xl my-4">
+					Outdoor <br /> ginger
+				</h2>
 			</div>
+			<p>
+				At just 19, August left Copenhagen on his bike, reaching the wild
+				landscapes of France, eschewing traditional schooling for adventure, and
+				teaching others in the process.
+			</p>
 		</article>
 	);
 }
