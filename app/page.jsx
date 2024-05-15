5db896133bc1e59579010437ba2bb920ebb8fc64
jaco266w <jaco266w@stud.kea.dk>
2024-05-15 10:21:21 +0200

    chore: Update profile section styling on home page

--- app/page.jsx
+++ app/page.jsx
@@ -31,41 +31,44 @@ export default function Home() {
 						Become a member
 					</Button>
 				</div>
-				<div id="right" className="absolute left-44 top-32">
-					<div className="flex gap-2 items-center mb-16 bg-[#F5F5F5]/75 px-1 py-1 rounded-full w-max">
+				<div
+					id="right"
+					className="absolute left-44 top-32 md:left-[1050px] md:top-[350px]"
+				>
+					<div className="flex gap-2 items-center mb-16 md:mb-40 bg-[#F5F5F5]/75 px-1 py-1 rounded-full w-max">
 						<Image
 							src="/profile.webp"
 							alt="August Vallant"
 							width={500}
 							height={500}
-							className="rounded-full aspect-square w-8 h-8"
+							className="rounded-full aspect-square w-8 h-8 md:w-11 md:h-11"
 						/>
-						<div className="pr-2 uppercase">
-							<p className="text-[0.7rem] font-medium text-ogPrimary">
+						<div className="pr-2 md:pr-4 uppercase">
+							<p className="text-[0.7rem] md:text-[1rem] font-medium md:font-semibold text-ogPrimary">
 								August Vallant
 							</p>
-							<p className="text-[0.6rem] font-light text-ogPrimary">
+							<p className="text-[0.6rem] md:text-[0.9rem] font-light text-ogPrimary">
 								Adventure Influencer
 							</p>
 						</div>
 					</div>
-					<div className="flex gap-2 items-center ml-24 bg-[#F5F5F5]/75 px-1 py-1 rounded-full w-max">
-						<div className="bg-ogPrimary rounded-full p-[5px]">
+					<div className="flex gap-2 items-center ml-24 md:ml-64 bg-[#F5F5F5]/75 px-1 py-1 rounded-full w-max">
+						<div className="bg-ogPrimary rounded-full p-[5px] md:p-[6px]">
 							<Image
 								src="/icons/instagramWhite.svg"
 								alt="August Vallant"
 								width={500}
 								height={500}
-								className="aspect-square w-5 h-5"
+								className="aspect-square w-5 h-5 md:w-7 md:h-7"
 							/>
 						</div>
 						<div className="pr-2 uppercase">
 							<AnimatedCount
 								finalCount={333000}
 								duration={3}
-								className="text-[1rem]"
+								className="text-[1rem] md:text-[1.4rem]"
 							/>
-							<p className="text-[0.45rem] font-light text-ogPrimary">
+							<p className="-mt-1 md:-mt-2 text-[0.45rem] md:text-[0.6rem] font-light text-ogPrimary">
 								Followers
 							</p>
 						</div>
