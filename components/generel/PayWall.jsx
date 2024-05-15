bc67caae98000842f8c493d3ad34019e44b74a81
jaco266w <jaco266w@stud.kea.dk>
2024-05-15 12:26:32 +0200

    feat: Improve PayWall component for better content visibility

--- components/generel/PayWall.jsx
+++ components/generel/PayWall.jsx
@@ -8,13 +8,25 @@ import { PortableText } from "next-sanity";
 export default function Paywall({ post }) {
 	const { user } = useAuthStore();
 
-	const freeContent = post[0].content.slice(0, 3);
+	let normalCount = 0;
+	const freeContent = [];
+
+	for (let i = 0; i < post[0].content.length; i++) {
+		freeContent.push(post[0].content[i]);
+		if (post[0].content[i].style === "normal") {
+			normalCount++;
+		}
+		if (normalCount === 2) {
+			break;
+		}
+	}
+	console.log(freeContent);
 
 	if (!user) {
 		return (
 			<>
 				<PortableText value={freeContent} />
-				<div className="-mt-28 relative z-10">
+				<div className="-mt-40 relative z-10">
 					<div className="h-28 bg-gradient-to-b from-transparent to-ogBG-base" />
 					<div className="bg-ogBG-base rounded-lg px-8 py-4 drop-shadow-xl">
 						<p className="text-xl capitalize text-ogLabel-title">
