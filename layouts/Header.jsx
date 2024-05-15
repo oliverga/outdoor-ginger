537c60b4628efe8e8665a82c408da79f0aec241f
Oliver G.A. <hi@oliverga.com>
2024-05-15 09:41:32 +0200

    x

--- layouts/Header.jsx
+++ layouts/Header.jsx
@@ -4,10 +4,10 @@ import { Button } from "@/components/ui/button";
 import { IconHeartHandshake, IconShoppingCart } from "@tabler/icons-react";
 import Link from "next/link";
 import {
-	motion,
-	useMotionValueEvent,
-	useScroll,
-	AnimatePresence,
+  motion,
+  useMotionValueEvent,
+  useScroll,
+  AnimatePresence,
 } from "framer-motion";
 import { useState, useEffect } from "react";
 import { usePathname } from "next/navigation";
@@ -15,144 +15,140 @@ import useAuthStore from "@/lib/store/authStore";
 import ShoppingCart from "@/components/Cart/ShoppingCart";
 
 function Header() {
-	const { user } = useAuthStore();
-	const [hidden, setHidden] = useState(false);
-	const [prevScroll, setPrevScroll] = useState(0);
-	const [cartOpen, setCartOpen] = useState(false);
+  const { user } = useAuthStore();
+  const [hidden, setHidden] = useState(false);
+  const [prevScroll, setPrevScroll] = useState(0);
+  const [cartOpen, setCartOpen] = useState(false);
 
-	const pathname = usePathname();
+  const pathname = usePathname();
 
-	const linkStyle = (path) =>
-		pathname === path ? "p-2 text-ogPrimary" : "p-2";
+  const linkStyle = (path) =>
+    pathname === path ? "p-2 text-ogPrimary" : "p-2";
 
-	const progress = "64%";
+  const progress = "64%";
 
-	const { scrollY } = useScroll();
+  const { scrollY } = useScroll();
 
-	const handleVisibility = (scrollPosition) => {
-		const shouldHide = scrollPosition > prevScroll && scrollPosition > 60;
-		setHidden(shouldHide);
-		if (shouldHide && cartOpen) {
-			setCartOpen(false); // Also close the cart when hiding the header
-		}
-		setPrevScroll(scrollPosition);
-	};
+  const handleVisibility = (scrollPosition) => {
+    const shouldHide = scrollPosition > prevScroll && scrollPosition > 60;
+    setHidden(shouldHide);
+    if (shouldHide && cartOpen) {
+      setCartOpen(false); // Also close the cart when hiding the header
+    }
+    setPrevScroll(scrollPosition);
+  };
 
-	useMotionValueEvent(scrollY, "change", handleVisibility);
+  useMotionValueEvent(scrollY, "change", handleVisibility);
 
-	useEffect(() => {
-		const handleMouseMove = (event) => {
-			if (event.clientY <= window.innerHeight / 3 && hidden) {
-				setHidden(false); // Show header if mouse is in the top third
-			}
-		};
-		window.addEventListener("mousemove", handleMouseMove);
-		return () => {
-			window.removeEventListener("mousemove", handleMouseMove);
-		};
-	}, [hidden]);
+  useEffect(() => {
+    const handleMouseMove = (event) => {
+      if (event.clientY <= window.innerHeight / 3 && hidden) {
+        setHidden(false); // Show header if mouse is in the top third
+      }
+    };
+    window.addEventListener("mousemove", handleMouseMove);
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, [hidden]);
 
-	const toggleCart = () => {
-		if (!cartOpen && hidden) {
-			setHidden(false); // Ensure header is visible when opening the cart
-		}
-		setCartOpen(!cartOpen);
-	};
+  const toggleCart = () => {
+    if (!cartOpen && hidden) {
+      setHidden(false); // Ensure header is visible when opening the cart
+    }
+    setCartOpen(!cartOpen);
+  };
 
-	return (
-		<header>
-			<motion.div
-				className="hidden md:block fixed top-0 left-0 z-60 w-screen px-8 py-4"
-				animate={{ y: hidden ? "-60%" : "0" }}
-				transition={{
-					duration: 0.8,
-					type: "spring",
-					bounce: 0.3,
-				}}
-			>
-				<div className="max-w-7xl overflow-hidden mx-auto  rounded-t-xl z-40 drop-shadow-xl">
-					<div className=" w-full h-20 bg-ogBG-base rounded-t-xl flex items-center justify-between px-8 border border-x-neutral-200 border-t-neutral-200 border-b-0 border-opacity-50 mx">
-						<motion.div
-							className="opacity-0"
-							animate={{ opacity: 1 }}
-							transition={{ duration: 2, type: "spring" }}
-						>
-							<Link href="/" className="flex">
-								<dotlottie-player
-									src="/lottie/iconText.lottie"
-									background="transparent"
-									speed="1"
-									style={{ width: "150px" }}
-									direction="1"
-									playMode="bounce"
-									loop
-									autoplay
-									hover
-									onClick={() => setCartOpen(false)}
-								></dotlottie-player>
-							</Link>
-						</motion.div>
-						<div className="flex gap-8 items-center w-full justify-end">
-							<nav className="w-full max-w-xl">
-								<ul className="flex w-full justify-between items-center text-sm font-normal whitespace-nowrap">
-									<li>
-										<Link
-											className={linkStyle("/membership")}
-											href="/membership"
-											onClick={() => setCartOpen(false)}
-										>
-											Membership
-										</Link>
-									</li>
-									<li>
-										<Link
-											className={linkStyle("/equipment")}
-											href="/equipment"
-											onClick={() => setCartOpen(false)}
-										>
-											Equipment
-										</Link>
-									</li>
-									<li>
-										<Link
-											className={linkStyle("/blog")}
-											href="/blog"
-											onClick={() => setCartOpen(false)}
-										>
-											Blog
-										</Link>
-									</li>
-									<li>
-										<Link
-											className={linkStyle("/about")}
-											href="/about"
-											onClick={() => setCartOpen(false)}
-										>
-											About me
-										</Link>
-									</li>
-									{user ? (
-										<li>
-											<Link
-												className={linkStyle("/profile")}
-												href="/profile"
-												onClick={() => setCartOpen(false)}
-											>
-												My Profile
-											</Link>
-										</li>
-									) : (
-										<li>
-											<Link
-												className={linkStyle("/signup")}
-												href="/login"
-												onClick={() => setCartOpen(false)}
-											>
-												<Button variant="outline">Log in</Button>
-											</Link>
-										</li>
-									)}
-									{/* <li>
+  return (
+    <header>
+      <motion.div
+        className="hidden md:block fixed top-0 left-0 z-60 w-screen px-8 py-4"
+        animate={{ y: hidden ? "-60%" : "0" }}
+        transition={{
+          duration: 0.8,
+          type: "spring",
+          bounce: 0.3,
+        }}
+      >
+        <div className="max-w-7xl overflow-hidden mx-auto  rounded-t-xl z-40 drop-shadow-xl">
+          <div className=" w-full h-20 bg-ogBG-base rounded-t-xl flex items-center justify-between px-8 border border-x-neutral-200 border-t-neutral-200 border-b-0 border-opacity-50 mx">
+            <motion.div
+              className="opacity-0"
+              animate={{ opacity: 1 }}
+              transition={{ duration: 2, type: "spring" }}
+            >
+              <Link href="/" className="flex">
+                <dotlottie-player
+                  src="/lottie/iconText.lottie"
+                  background="transparent"
+                  speed="1"
+                  style={{ width: "150px" }}
+                  direction="1"
+                  playMode="bounce"
+                  loop
+                  autoplay
+                  hover
+                  onClick={() => setCartOpen(false)}
+                ></dotlottie-player>
+              </Link>
+            </motion.div>
+            <div className="flex gap-8 items-center w-full justify-end">
+              <nav className="w-full max-w-xl">
+                <ul className="flex w-full justify-between items-center text-sm font-normal whitespace-nowrap">
+                  <li>
+                    <Link
+                      className={linkStyle("/membership")}
+                      href="/membership"
+                      onClick={() => setCartOpen(false)}
+                    >
+                      Membership
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      className={linkStyle("/equipment")}
+                      href="/equipment"
+                      onClick={() => setCartOpen(false)}
+                    >
+                      Equipment
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      className={linkStyle("/blog")}
+                      href="/blog"
+                      onClick={() => setCartOpen(false)}
+                    >
+                      Blog
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      className={linkStyle("/about")}
+                      href="/about"
+                      onClick={() => setCartOpen(false)}
+                    >
+                      About me
+                    </Link>
+                  </li>
+                  {user ? (
+                    <li>
+                      <Link
+                        className={linkStyle("/profile")}
+                        href="/profile"
+                        onClick={() => setCartOpen(false)}
+                      >
+                        My Profile
+                      </Link>
+                    </li>
+                  ) : (
+                    <li>
+                      <Link href="/login" onClick={() => setCartOpen(false)}>
+                        <Button variant="outline">Log in</Button>
+                      </Link>
+                    </li>
+                  )}
+                  {/* <li>
                     <Link
                       className={linkStyle("/about#contact")}
                       href="/about#contact"
@@ -161,65 +157,65 @@ function Header() {
                       Contact
                     </Link>
                   </li> */}
-								</ul>
-							</nav>
-							<IconShoppingCart
-								size={24}
-								className={`stroke-[1.5px] cursor-pointer ${cartOpen ? "text-ogPrimary" : "text-ogLabel-base "}`}
-								onClick={toggleCart}
-							/>
-						</div>
-					</div>
-				</div>
-				<div className="flex max-w-7xl mx-auto drop-shadow-xl">
-					<div className="w-full">
-						<div className=" h-3 w-full mx-auto flex bg-ogBG-sub relative z-30 rounded-bl-xl overflow-hidden ">
-							<motion.div
-								className="w-0 h-3 bg-ogPrimary absolute top-0 left-0 "
-								animate={{ width: progress }}
-								transition={{ duration: 1.8, type: "spring" }}
-							></motion.div>
-						</div>
-						<div className=" w-full mx-auto flex h-full">
-							<motion.div
-								className="w-0 h-0 opacity-0"
-								animate={{ width: progress }}
-								transition={{ duration: 1.8, type: "spring" }}
-							></motion.div>
-							<motion.div
-								className=" origin-right -translate-x-full -translate-y-[1px] z-50 bg-ogPrimary text-ogBG-base h-fit p-2 rounded-b-xl text-sm font-medium opacity-0"
-								animate={{ opacity: 1 }}
-								transition={{ duration: 1.8, type: "spring" }}
-							>
-								<p className=" cursor-default text-ogBG-base">{progress}</p>
-							</motion.div>
-						</div>
-					</div>
-					<div className="w-fit h-fit  mx-auto flex justify-end">
-						<div className="w-fit h-full max-w-xs bg-ogBG-sub justify-self-end rounded-b-xl flex justify-between gap-4 items-center px-2 py-2  z-30 ">
-							<p className=" whitespace-nowrap text-sm font-normal cursor-default text-ogLabel-muted pl-2">
-								Goal: 10.000 kr.
-							</p>
-							<Link
-								href="https://www.gofundme.com/f/outdoorginger"
-								target="_blank"
-							>
-								<Button
-									variant="primary"
-									size="sm"
-									className="bg-ogPrimary hover:bg-ogPrimary-dark gap-1"
-								>
-									Donate
-									<IconHeartHandshake size={20} className="stroke-[1.5px]" />
-								</Button>
-							</Link>
-						</div>
-					</div>
-				</div>
-			</motion.div>
-			<AnimatePresence>{cartOpen && <ShoppingCart />}</AnimatePresence>
-		</header>
-	);
+                </ul>
+              </nav>
+              <IconShoppingCart
+                size={24}
+                className={`stroke-[1.5px] cursor-pointer ${cartOpen ? "text-ogPrimary" : "text-ogLabel-base "}`}
+                onClick={toggleCart}
+              />
+            </div>
+          </div>
+        </div>
+        <div className="flex max-w-7xl mx-auto drop-shadow-xl">
+          <div className="w-full">
+            <div className=" h-3 w-full mx-auto flex bg-ogBG-sub relative z-30 rounded-bl-xl overflow-hidden ">
+              <motion.div
+                className="w-0 h-3 bg-ogPrimary absolute top-0 left-0 "
+                animate={{ width: progress }}
+                transition={{ duration: 1.8, type: "spring" }}
+              ></motion.div>
+            </div>
+            <div className=" w-full mx-auto flex h-full">
+              <motion.div
+                className="w-0 h-0 opacity-0"
+                animate={{ width: progress }}
+                transition={{ duration: 1.8, type: "spring" }}
+              ></motion.div>
+              <motion.div
+                className=" origin-right -translate-x-full -translate-y-[1px] z-50 bg-ogPrimary text-ogBG-base h-fit p-2 rounded-b-xl text-sm font-medium opacity-0"
+                animate={{ opacity: 1 }}
+                transition={{ duration: 1.8, type: "spring" }}
+              >
+                <p className=" cursor-default text-ogBG-base">{progress}</p>
+              </motion.div>
+            </div>
+          </div>
+          <div className="w-fit h-fit  mx-auto flex justify-end">
+            <div className="w-fit h-full max-w-xs bg-ogBG-sub justify-self-end rounded-b-xl flex justify-between gap-4 items-center px-2 py-2  z-30 ">
+              <p className=" whitespace-nowrap text-sm font-normal cursor-default text-ogLabel-muted pl-2">
+                Goal: 10.000 kr.
+              </p>
+              <Link
+                href="https://www.gofundme.com/f/outdoorginger"
+                target="_blank"
+              >
+                <Button
+                  variant="primary"
+                  size="sm"
+                  className="bg-ogPrimary hover:bg-ogPrimary-dark gap-1"
+                >
+                  Donate
+                  <IconHeartHandshake size={20} className="stroke-[1.5px]" />
+                </Button>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </motion.div>
+      <AnimatePresence>{cartOpen && <ShoppingCart />}</AnimatePresence>
+    </header>
+  );
 }
 
 export default Header;
