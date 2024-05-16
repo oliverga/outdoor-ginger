1ba98fc635dc57b24b6455521b66fdb36ff44153
Oliver G.A. <hi@oliverga.com>
2024-05-16 15:48:28 +0200

    x

--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -6,25 +6,31 @@ import Globe from "@/components/generel/Globe";
 import BlogOverview from "@/components/generel/BlogOverview";
 
 const postQuery =
-	'*[_type == "post"]{title, slug, author, publishedAt, mainImage, content, Latitude, Longitude}';
+  '*[_type == "post"]{title, slug, author, publishedAt, mainImage, content, Latitude, Longitude}';
 
 const posts = await client.fetch(postQuery, {
-	next: {
-		revalidate: process.env.NODE_ENV === "development" ? 30 : 3600,
-	},
+  next: {
+    revalidate: process.env.NODE_ENV === "development" ? 30 : 3600,
+  },
 });
 
 export default function Page() {
-	return (
-		<main>
-			<Hero
-				title="Campfire Chronicles"
-				imageSrc="/bloghero.webp"
-				height="h-96 md:h-[50vh]"
-			></Hero>
-			<Globe posts={posts} />
-			<BlogOverview posts={posts} />
-			<NewsletterBanner />
-		</main>
-	);
+  return (
+    <main>
+      <Hero
+        title="Campfire Chronicles"
+        imageSrc="/bloghero.webp"
+        height="h-96 md:h-[50vh]"
+      >
+        <div id="left" className="w-[200%] md:w-full md:mb-12">
+          <p className="text-xl md:text-2xl font-medium opacity-70 font-display text-ogBG-base">
+            The Outdoor Ginger Blog
+          </p>
+        </div>
+      </Hero>
+      <Globe posts={posts} />
+      <BlogOverview posts={posts} />
+      <NewsletterBanner />
+    </main>
+  );
 }
