63b44ff62d0321b33718d4b6e7c8c73288d78f68
Oliver G.A. <hi@oliverga.com>
2024-05-16 09:48:18 +0200

    forside hero

--- app/page.jsx
+++ app/page.jsx
@@ -12,84 +12,86 @@ import AnimatedCount from "@/components/generel/AnimatedCount";
 import Link from "next/link";
 
 export default function Home() {
-	return (
-		<main>
-			<Hero title="Outdoor Ginger" imageSrc="/forsidehero.webp" bgPos="bottom">
-				<div id="left" className="mt-4 -mb-6">
-					<div className="hidden md:block font-light w-144 my-8">
-						<p className="text-ogBG-base">
-							August Vallant is an outdoor influencer who takes it to the next
-							level.
-						</p>
-						<p className="text-ogBG-base">Join him on his next adventure!</p>
-					</div>
-					<Link href="/membership">
-						<Button size="md" variant="primary" className="capitalize md:mb-12">
-							Become a member
-						</Button>
-					</Link>
-				</div>
-				<div
-					id="right"
-					className="absolute left-44 top-32 md:left-[1050px] md:top-[350px]"
-				>
-					<div className="flex gap-2 items-center mb-16 md:mb-40 bg-[#F5F5F5]/75 px-1 py-1 rounded-full w-max">
-						<Image
-							src="/profile.webp"
-							alt="August Vallant"
-							width={500}
-							height={500}
-							className="rounded-full aspect-square w-8 h-8 md:w-11 md:h-11"
-						/>
-						<div className="pr-2 md:pr-4 uppercase">
-							<p className="text-[0.7rem] md:text-[1rem] font-semibold text-ogPrimary">
-								August Vallant
-							</p>
-							<p className="text-[0.6rem] md:text-[0.9rem] font-light text-ogPrimary">
-								Adventure Influencer
-							</p>
-						</div>
-					</div>
-					<div className="flex gap-2 items-center ml-24 md:ml-64 bg-[#F5F5F5]/75 px-1 py-1 rounded-full w-max">
-						<div className="bg-ogPrimary rounded-full p-[5px] md:p-[6px]">
-							<Image
-								src="/icons/instagramWhite.svg"
-								alt="August Vallant"
-								width={500}
-								height={500}
-								className="aspect-square w-5 h-5 md:w-7 md:h-7"
-							/>
-						</div>
-						<div className="pr-2 uppercase">
-							<AnimatedCount
-								finalCount={333000}
-								duration={3}
-								className="text-[1rem] md:text-[1.4rem]"
-							/>
-							<p className="-mt-1 md:-mt-2 text-[0.45rem] md:text-[0.6rem] font-light text-ogPrimary">
-								Followers
-							</p>
-						</div>
-					</div>
-				</div>
-			</Hero>
-			<div className="space-y-32">
-				<div>
-					<AboutBlock />
-				</div>
-				<div>
-					<LatestYT />
-				</div>
-				<div>
-					<JoinTheClub />
-				</div>
-				<div>
-					<SocialFollowers />
-					<SponsorBanner />
-					<SupportBanner type="original" />
-					<NewsletterBanner />
-				</div>
-			</div>
-		</main>
-	);
+  return (
+    <main>
+      <Hero title="Outdoor Ginger" imageSrc="/forsidehero.webp" bgPos="bottom">
+        <div id="left" className="mt-4 -mb-6">
+          <div className="hidden md:block font-light w-144 mb-8 mt-4">
+            <p className="text-ogBG-base">
+              August Vallat is an outdoor influencer who takes it to the next
+              level.
+            </p>
+            <p className="text-ogBG-base mt-2">
+              Join him on his next adventure!
+            </p>
+          </div>
+          <Link href="/membership">
+            <Button size="md" variant="primary" className="capitalize md:mb-12">
+              Become a member
+            </Button>
+          </Link>
+        </div>
+        <div
+          id="right"
+          className="absolute left-44 top-32 md:left-[1050px] md:top-[350px]"
+        >
+          <div className="flex gap-2 items-center mb-16 md:mb-40 bg-[#F5F5F5]/75 px-1 py-1 rounded-full w-max">
+            <Image
+              src="/profile.webp"
+              alt="August Vallant"
+              width={500}
+              height={500}
+              className="rounded-full aspect-square w-8 h-8 md:w-11 md:h-11"
+            />
+            <div className="pr-2 md:pr-4 uppercase">
+              <p className="text-[10px] md:text-[1rem] font-semibold text-ogPrimary">
+                August Vallat
+              </p>
+              <p className="text-[9px] md:text-[13px] font-light text-ogPrimary-light">
+                Adventure Influencer
+              </p>
+            </div>
+          </div>
+          <div className="flex gap-2 items-center ml-24 md:ml-64 bg-[#F5F5F5]/75 px-1 py-1 rounded-full w-max">
+            <div className="bg-ogPrimary rounded-full p-[5px] md:p-[6px]">
+              <Image
+                src="/icons/instagramWhite.svg"
+                alt="August Vallant"
+                width={500}
+                height={500}
+                className="aspect-square w-5 h-5 md:w-7 md:h-7"
+              />
+            </div>
+            <div className="pr-2 uppercase">
+              <AnimatedCount
+                finalCount={333000}
+                duration={3}
+                className="text-base md:text-[1.4rem]"
+              />
+              <p className="-mt-1 md:-mt-2 text-[7px] md:text-[0.6rem] font-light text-ogPrimary">
+                Followers
+              </p>
+            </div>
+          </div>
+        </div>
+      </Hero>
+      <div className="space-y-32">
+        <div>
+          <AboutBlock />
+        </div>
+        <div>
+          <LatestYT />
+        </div>
+        <div>
+          <JoinTheClub />
+        </div>
+        <div>
+          <SocialFollowers />
+          <SponsorBanner />
+          <SupportBanner type="original" />
+          <NewsletterBanner />
+        </div>
+      </div>
+    </main>
+  );
 }
--- components/generel/Hero.jsx
+++ components/generel/Hero.jsx
@@ -2,20 +2,36 @@ import React from "react";
 import Image from "next/image";
 
 function Hero({ title, imageSrc, height = "h-96 md:h-[80vh]", children }) {
-  const leftSideChildren = React.Children.toArray(children).filter((child) => child.props.id === "left");
-  const rightSideChildren = React.Children.toArray(children).filter((child) => child.props.id === "right");
+  const leftSideChildren = React.Children.toArray(children).filter(
+    (child) => child.props.id === "left",
+  );
+  const rightSideChildren = React.Children.toArray(children).filter(
+    (child) => child.props.id === "right",
+  );
   return (
     <section className="relative mb-6 ">
-      <div className={`overflow-hidden grid grid-cols-2 max-w-5xl mx-auto pb-24 px-8 ${height}`}>
+      <div
+        className={`overflow-hidden grid grid-cols-2 max-w-5xl mx-auto pb-24 px-8 ${height}`}
+      >
         <div className="place-self-end z-30 w-full flex flex-col justify-end">
-          <h1 className="text-4xl md:text-7xl font-bold text-ogBG-base leading-7 pb-4 uppercase m-0">{title}</h1>
-          {leftSideChildren}
+          <h1 className="text-4xl md:text-7xl font-bold text-ogBG-base uppercase m-0">
+            {title}
+          </h1>
+          <div>{leftSideChildren}</div>
         </div>
         <div className="z-40 place-self-center">{rightSideChildren}</div>
       </div>
       <div className="rounded-t-[3rem] bg-ogBG-base col-span-2 h-12 z-30 absolute bottom-0 left-0 w-full"></div>
       <div className=" bg-gradient-to-tr from-ogLabel-base to-transparent  w-full h-full absolute top-0 left-0 z-20 opacity-75"></div>
-      <Image src={imageSrc} alt="" width={920} height={500} className="object-cover object-center w-full h-full absolute top-0 left-0 z-10" placeholder="blur" blurDataURL={`{imageSrc}?blur=50`} />
+      <Image
+        src={imageSrc}
+        alt=""
+        width={920}
+        height={500}
+        className="object-cover object-center w-full h-full absolute top-0 left-0 z-10"
+        placeholder="blur"
+        blurDataURL={`{imageSrc}?blur=50`}
+      />
     </section>
   );
 }
