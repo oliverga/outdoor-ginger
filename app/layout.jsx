6e4decaf24b5dacf4407bed721a8b8f7652d670e
jaco266w <jaco266w@stud.kea.dk>
2024-05-16 10:23:30 +0200

    chore: Update npm dependencies and add Mapbox GL and React Map GL

--- app/layout.jsx
+++ app/layout.jsx
@@ -9,39 +9,45 @@ import CookieConsentDialog from "@/components/CookiesConsentDialog";
 import AuthSubscriber from "@/components/AuthSubscriber";
 
 const inter = Inter({
-  subsets: ["latin"],
-  display: "swap",
-  variable: "--font-inter",
+	subsets: ["latin"],
+	display: "swap",
+	variable: "--font-inter",
 });
 const syne = Syne({
-  subsets: ["latin"],
-  display: "swap",
-  variable: "--font-syne",
+	subsets: ["latin"],
+	display: "swap",
+	variable: "--font-syne",
 });
 
 export default async function RootLayout({ children }) {
-  return (
-    <html lang="en" className={`${inter.variable} ${syne.variable}`}>
-      <body className="bg-ogBG-base text-ogLabel-base">
-        <MobileHeader />
-        <Header />
-        <AuthSubscriber />
-        <Toaster
-          richColors={true}
-          toastOptions={{
-            style: {
-              background: "oklch(var(--og-bg-base))",
-              color: "oklch(var(--og-label-base))",
-              border: "1px solid oklch(var(--og-border-input))",
-              borderRadius: "var(--og-radius-mobile)",
-            },
-          }}
-          theme="light"
-        />
-        <CookieConsentDialog />
-        {children}
-        <Footer />
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en" className={`${inter.variable} ${syne.variable}`}>
+			<head>
+				<link
+					href="https://api.mapbox.com/mapbox-gl-js/v2.6.1/mapbox-gl.css"
+					rel="stylesheet"
+				/>
+			</head>
+			<body className="bg-ogBG-base text-ogLabel-base">
+				<MobileHeader />
+				<Header />
+				<AuthSubscriber />
+				<Toaster
+					richColors={true}
+					toastOptions={{
+						style: {
+							background: "oklch(var(--og-bg-base))",
+							color: "oklch(var(--og-label-base))",
+							border: "1px solid oklch(var(--og-border-input))",
+							borderRadius: "var(--og-radius-mobile)",
+						},
+					}}
+					theme="light"
+				/>
+				<CookieConsentDialog />
+				{children}
+				<Footer />
+			</body>
+		</html>
+	);
 }
