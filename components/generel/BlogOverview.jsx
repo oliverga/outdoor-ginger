926649b132ceadc057cf07a990d274febe42284b
Oliver G.A. <hi@oliverga.com>
2024-05-17 12:14:40 +0200

    blog globe thangs

--- components/generel/BlogOverview.jsx
+++ components/generel/BlogOverview.jsx
@@ -7,59 +7,61 @@ import { urlFor } from "@/lib/sanity/client";
 import { usePostStore } from "@/lib/store/postStore";
 
 export default function BlogOverview({ posts }) {
-	const { post, setPost } = usePostStore();
+  const { post, setPost } = usePostStore();
 
-	return (
-		<section className="pb-32 max-w-5xl mx-auto px-8 md:-mt-[600px] md:relative md:z-60">
-			<div className="h-28 bg-gradient-to-b from-transparent to-ogBG-base hidden md:block" />
-			<div className="flex flex-col md:grid grid-cols-3 gap-6 bg-ogBG-base">
-				<div className=" col-span-1 flex flex-col gap-6 -mt-6 md:mt-0">
-					{posts.slice(0, Math.ceil(posts.length / 2)).map((post) => {
-						return <Article post={post} key={post.slug.current} />;
-					})}
-				</div>
-				<div className="col-span-2 flex flex-col gap-6">
-					{posts.slice(Math.ceil(posts.length / 2)).map((post) => {
-						return <Article post={post} key={post.slug.current} />;
-					})}
-				</div>
-			</div>
-		</section>
-	);
+  return (
+    <div className="pb-32 max-w-5xl mx-auto px-8 md:-mt-[500px] pointer-events-auto">
+      <div className="h-28 bg-gradient-to-b from-transparent to-ogBG-base hidden md:block relative pointer-events-none z-60 " />
+      <section className="md:relative md:z-60 pointer-events-auto">
+        <div className="flex flex-col md:grid grid-cols-3 gap-6 bg-ogBG-base ">
+          <div className=" col-span-1 flex flex-col gap-6 -mt-6 md:mt-0">
+            {posts.slice(0, Math.ceil(posts.length / 2)).map((post) => {
+              return <Article post={post} key={post.slug.current} />;
+            })}
+          </div>
+          <div className="col-span-2 flex flex-col gap-6">
+            {posts.slice(Math.ceil(posts.length / 2)).map((post) => {
+              return <Article post={post} key={post.slug.current} />;
+            })}
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 }
 
 function Article({ post }) {
-	const { setPost } = usePostStore();
-	return (
-		<div
-			className="@container"
-			onMouseEnter={() => setPost(post)}
-			onMouseLeave={() => setPost(null)}
-		>
-			<article
-				key={post.slug.current}
-				className="mx-auto border rounded-xl overflow-hidden w-full shadow-sm"
-			>
-				<Link href={`/blog/${post.slug.current}`}>
-					{post.mainImage && (
-						<div className="overflow-hidden">
-							<Image
-								src={urlFor(post.mainImage).url()}
-								alt=""
-								width={800}
-								height={400}
-								className="aspect-video @xl:aspect-[16/7] object-cover object-center w-full hover:scale-[102%] transition-all duration-500"
-							/>
-						</div>
-					)}
-					<div className="m-4 space-y-2">
-						<h2 className=" font-medium text-xl">{post.title}</h2>
-						<p className="text-xs text-ogLabel-faint">
-							{format(new Date(post.publishedAt), "PPP")}
-						</p>
-					</div>
-				</Link>
-			</article>
-		</div>
-	);
+  const { setPost } = usePostStore();
+  return (
+    <div
+      className="@container"
+      onMouseEnter={() => setPost(post)}
+      onMouseLeave={() => setPost(null)}
+    >
+      <article
+        key={post.slug.current}
+        className="mx-auto border rounded-xl overflow-hidden w-full shadow-sm"
+      >
+        <Link href={`/blog/${post.slug.current}`}>
+          {post.mainImage && (
+            <div className="overflow-hidden">
+              <Image
+                src={urlFor(post.mainImage).url()}
+                alt=""
+                width={800}
+                height={400}
+                className="aspect-video @xl:aspect-[16/7] object-cover object-center w-full hover:scale-[102%] transition-all duration-500"
+              />
+            </div>
+          )}
+          <div className="m-4 space-y-2">
+            <h2 className=" font-medium text-xl">{post.title}</h2>
+            <p className="text-xs text-ogLabel-faint">
+              {format(new Date(post.publishedAt), "PPP")}
+            </p>
+          </div>
+        </Link>
+      </article>
+    </div>
+  );
 }
