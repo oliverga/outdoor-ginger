3d83022e0ee35224e8788abb438928dbbdece6e9
Oliver G.A. <hi@oliverga.com>
2024-05-17 12:47:05 +0200

    x

--- components/generel/MemberBanner.jsx
+++ components/generel/MemberBanner.jsx
@@ -3,7 +3,7 @@ import { Button } from "../ui/button";
 
 export default function MemberBanner({ title, description, buttonText }) {
   return (
-    <article className="bg-ogPrimary-darkest px-8 py-24 flex flex-col gap-12 items-center">
+    <article className="bg-ogPrimary-dark px-8 py-24 flex flex-col gap-12 items-center">
       <h2 className="font-display text-5xl font-bold text-ogPrimary-lightest uppercase">
         {title}
       </h2>
