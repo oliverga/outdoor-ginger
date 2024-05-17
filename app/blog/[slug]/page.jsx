a603d10220bfff8f0559786494247e2bb1bfabaf
Oliver G.A. <hi@oliverga.com>
2024-05-17 12:42:02 +0200

    x

--- app/blog/[slug]/page.jsx
+++ app/blog/[slug]/page.jsx
@@ -124,7 +124,7 @@ export default async function Page({ params }) {
           )}
         </div>
       </div>
-      <NewsletterBanner />
+      <NewsletterBanner title="Stay up to date" subtitle="" />
     </main>
   );
 }
