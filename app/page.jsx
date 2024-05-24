9acde2e27277b84985f5c5d00a63250809fa128b
jaco266w <jaco266w@stud.kea.dk>
2024-05-24 12:15:16 +0200

    chore: Refactor Hero component to improve readability and add image priority

--- app/page.jsx
+++ app/page.jsx
@@ -12,91 +12,91 @@ import AnimatedCount from "@/components/generel/AnimatedCount";
 import Link from "next/link";
 
 export default function Home() {
-  return (
-    <main>
-      <Hero title="Outdoor Ginger" bg="bg-forside2">
-        <div
-          id="left"
-          className="hidden md:flex flex-col w-full gap-8 mt-8 text-lg"
-        >
-          <p className="text-ogBG-base max-w-[80%] opacity-80 leading-normal text-lg">
-            I&apos;m an outdoor influencer who takes it to the next level.{" "}
-            <br />
-            Join me on my next adventure!
-          </p>
-          <Link href="/membership">
-            <Button size="md" variant="primary" className=" ">
-              Become a member
-            </Button>
-          </Link>
-        </div>
+	return (
+		<main>
+			<Hero title="Outdoor Ginger" imageSrc="/forsidehero2.webp">
+				<div
+					id="left"
+					className="hidden md:flex flex-col w-full gap-8 mt-8 text-lg"
+				>
+					<p className="text-ogBG-base max-w-[80%] opacity-80 leading-normal text-lg">
+						I&apos;m an outdoor influencer who takes it to the next level.{" "}
+						<br />
+						Join me on my next adventure!
+					</p>
+					<Link href="/membership">
+						<Button size="md" variant="primary" className=" ">
+							Become a member
+						</Button>
+					</Link>
+				</div>
 
-        <div
-          id="right"
-          className="w-[110%] md:w-full mb-8 md:mb-0 flex flex-col gap-6 md:gap-32 p-8 md:p-12"
-        >
-          <div className="flex gap-2 items-center  bg-neutral-100/75 px-2 py-2 rounded-full w-max">
-            <Image
-              src="/profile.webp"
-              alt="August Vallant"
-              width={500}
-              height={500}
-              className="rounded-full aspect-square w-8 h-8 md:w-11 md:h-11"
-            />
-            <div className="pr-2 md:pr-4 uppercase">
-              <p className="text-[10px] md:text-base font-semibold text-ogPrimary ">
-                August Vallat
-              </p>
-              <p className="text-[9px] md:text-[13px] font-normal text-ogLabel-base">
-                Adventure Influencer
-              </p>
-            </div>
-          </div>
-          <div className="flex gap-2 items-center  bg-neutral-100/75 px-4 py-3 rounded-full w-fit  place-self-end">
-            <div className="bg-ogPrimary rounded-full p-1 md:p-3 ">
-              <Link href="https://www.instagram.com/outdoorgingerchannel/">
-                <Image
-                  src="/icons/instagramWhite.svg"
-                  alt="Instagram"
-                  width={500}
-                  height={500}
-                  className="aspect-square w-5 h-5 md:w-7 md:h-7"
-                />
-              </Link>
-            </div>
-            <div className=" uppercase ">
-              <AnimatedCount
-                finalCount={333000}
-                duration={3}
-                className="text-base md:text-[1.4rem] p-0"
-              />
-              <p className="text-[9px] md:text-[13px] font-normal text-ogLabel-base">
-                Followers
-              </p>
-            </div>
-          </div>
-        </div>
-      </Hero>
-      <div className="space-y-24">
-        <div>
-          <AboutBlock />
-        </div>
-        <div>
-          <LatestYT />
-        </div>
-        <div>
-          <JoinTheClub />
-        </div>
-        <div>
-          <SocialFollowers />
-          <SponsorBanner />
-          <SupportBanner type="original" />
-          <NewsletterBanner
-            title="Campfire Chronicles"
-            subtitle="The Outdoor Ginger Blog"
-          />
-        </div>
-      </div>
-    </main>
-  );
+				<div
+					id="right"
+					className="w-[110%] md:w-full mb-8 md:mb-0 flex flex-col gap-6 md:gap-32 p-8 md:p-12"
+				>
+					<div className="flex gap-2 items-center  bg-neutral-100/75 px-2 py-2 rounded-full w-max">
+						<Image
+							src="/profile.webp"
+							alt="August Vallant"
+							width={500}
+							height={500}
+							className="rounded-full aspect-square w-8 h-8 md:w-11 md:h-11"
+						/>
+						<div className="pr-2 md:pr-4 uppercase">
+							<p className="text-[10px] md:text-base font-semibold text-ogPrimary ">
+								August Vallat
+							</p>
+							<p className="text-[9px] md:text-[13px] font-normal text-ogLabel-base">
+								Adventure Influencer
+							</p>
+						</div>
+					</div>
+					<div className="flex gap-2 items-center  bg-neutral-100/75 px-4 py-3 rounded-full w-fit  place-self-end">
+						<div className="bg-ogPrimary rounded-full p-1 md:p-3 ">
+							<Link href="https://www.instagram.com/outdoorgingerchannel/">
+								<Image
+									src="/icons/instagramWhite.svg"
+									alt="Instagram"
+									width={500}
+									height={500}
+									className="aspect-square w-5 h-5 md:w-7 md:h-7"
+								/>
+							</Link>
+						</div>
+						<div className=" uppercase ">
+							<AnimatedCount
+								finalCount={333000}
+								duration={3}
+								className="text-base md:text-[1.4rem] p-0"
+							/>
+							<p className="text-[9px] md:text-[13px] font-normal text-ogLabel-base">
+								Followers
+							</p>
+						</div>
+					</div>
+				</div>
+			</Hero>
+			<div className="space-y-24">
+				<div>
+					<AboutBlock />
+				</div>
+				<div>
+					<LatestYT />
+				</div>
+				<div>
+					<JoinTheClub />
+				</div>
+				<div>
+					<SocialFollowers />
+					<SponsorBanner />
+					<SupportBanner type="original" />
+					<NewsletterBanner
+						title="Campfire Chronicles"
+						subtitle="The Outdoor Ginger Blog"
+					/>
+				</div>
+			</div>
+		</main>
+	);
 }
--- components/generel/Hero.jsx
+++ components/generel/Hero.jsx
@@ -2,47 +2,48 @@ import React from "react";
 import Image from "next/image";
 
 function Hero({
-  title,
-  imageSrc,
-  height = "h-96 md:h-[80vh]",
-  bgPos = "bg-center",
-  bg,
-  children,
+	title,
+	imageSrc,
+	height = "h-96 md:h-[80vh]",
+	bgPos = "bg-center",
+	bg,
+	children,
 }) {
-  const leftSideChildren = React.Children.toArray(children).filter(
-    (child) => child.props.id === "left",
-  );
-  const rightSideChildren = React.Children.toArray(children).filter(
-    (child) => child.props.id === "right",
-  );
-  return (
-    <section className={`relative mb-6 ${bg} ${bgPos} bg-cover px-8 md:px-8 `}>
-      <div
-        className={`overflow-hidden grid grid-cols-2 max-w-7xl mx-auto pb-20 md:pb-32 ${height}`}
-      >
-        <div className="place-self-end z-30 w-full flex flex-col justify-end h-fit">
-          <h1 className="text-4xl md:text-7xl font-bold text-ogBG-base uppercase m-0 tracking-tight leading-snug">
-            {title}
-          </h1>
-          {leftSideChildren}
-        </div>
-        <div className="z-40 place-self-end w-full">{rightSideChildren}</div>
-      </div>
-      <div className="rounded-t-[3rem] bg-ogBG-base col-span-2 h-12 z-30 absolute bottom-0 left-0 w-full"></div>
-      <div className=" bg-gradient-to-tr from-ogLabel-base to-transparent  w-full h-full absolute top-0 left-0 z-20 opacity-75"></div>
-      {!bg && (
-        <Image
-          src={imageSrc}
-          alt=""
-          width={920}
-          height={500}
-          className={`object-cover ${bgPos} w-full h-full absolute top-0 left-0 z-10 outline`}
-          placeholder="blur"
-          blurDataURL={`${imageSrc}?blur=50`}
-        />
-      )}
-    </section>
-  );
+	const leftSideChildren = React.Children.toArray(children).filter(
+		(child) => child.props.id === "left"
+	);
+	const rightSideChildren = React.Children.toArray(children).filter(
+		(child) => child.props.id === "right"
+	);
+	return (
+		<section className={`relative mb-6 ${bg} ${bgPos} bg-cover px-8 md:px-8 `}>
+			<div
+				className={`overflow-hidden grid grid-cols-2 max-w-7xl mx-auto pb-20 md:pb-32 ${height}`}
+			>
+				<div className="place-self-end z-30 w-full flex flex-col justify-end h-fit">
+					<h1 className="text-4xl md:text-7xl font-bold text-ogBG-base uppercase m-0 tracking-tight leading-snug">
+						{title}
+					</h1>
+					{leftSideChildren}
+				</div>
+				<div className="z-40 place-self-end w-full">{rightSideChildren}</div>
+			</div>
+			<div className="rounded-t-[3rem] bg-ogBG-base col-span-2 h-12 z-30 absolute bottom-0 left-0 w-full"></div>
+			<div className=" bg-gradient-to-tr from-ogLabel-base to-transparent  w-full h-full absolute top-0 left-0 z-20 opacity-75"></div>
+			{!bg && (
+				<Image
+					src={imageSrc}
+					alt=""
+					width={1920}
+					height={1080}
+					className={`object-cover ${bgPos} w-full h-full absolute top-0 left-0 z-10 outline`}
+					placeholder="blur"
+					blurDataURL={`${imageSrc}?blur=50`}
+					priority={true}
+				/>
+			)}
+		</section>
+	);
 }
 
 export default Hero;
