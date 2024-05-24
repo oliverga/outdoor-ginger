2f3b22ab2faa6dd3b41693940da958c22cfc3647
jaco266w <jaco266w@stud.kea.dk>
2024-05-24 11:16:09 +0200

    chore: Remove commented out CookieConsentDialog component from layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -45,7 +45,7 @@ export default async function RootLayout({ children }) {
 					}}
 					theme="light"
 				/>
-				<CookieConsentDialog />
+				{/* <CookieConsentDialog /> */}
 				{children}
 				<Footer />
 				<Donation />
