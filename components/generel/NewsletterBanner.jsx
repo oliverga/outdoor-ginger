cf7dfe1f3eb2e6e26904b4914bd0593495bda5af
jaco266w <jaco266w@stud.kea.dk>
2024-05-27 10:12:49 +0200

    chore: Update npm dependencies and add email sending functionality

--- components/generel/NewsletterBanner.jsx
+++ components/generel/NewsletterBanner.jsx
@@ -1,40 +1,63 @@
+"use client";
+
 import { Button } from "../ui/button";
 import { Input } from "../ui/input";
+import { sendEmail as sendMail } from "../../lib/sendMail.js";
+import { useState, useEffect } from "react";
 
 function NewsletterBanner({ title, subtitle }) {
-  return (
-    <section className="h-fit bg-ogPrimary flex flex-col items-center gap-12 text-center px-4 py-16 md:py-24">
-      <div className=" space-y-4">
-        <h1 className="font-display font-bold uppercase  text-4xl md:text-7xl text-ogPrimary-lightest m-0">
-          {title}
-        </h1>
-        <h2 className=" font-display text-ogPrimary-lighter text-2xl font-medium m-0">
-          {subtitle}
-        </h2>
-      </div>
-      <p className="text-ogPrimary-lightest max-w-lg text-base md:text-lg leading-relaxed">
-        Subscribe to get interesting stories, field reports, and other goodies
-        straight to your inbox.
-      </p>
-      <form
-        action=""
-        className="flex flex-col md:flex-row gap-4 items-center w-full max-w-xs md:max-w-md"
-      >
-        <Input
-          type="email"
-          placeholder="Your email address"
-          className="bg-ogPrimary placeholder:text-ogPrimary-lightest text-ogPrimary-lightest border-ogPrimary-lighter focus-visible:ring-0 focus-visible:ring-offset-0"
-          required
-        />
-        <Button
-          size="lg"
-          className="bg-ogPrimary-dark text-ogBG-base hover:bg-ogPrimary-darker"
-        >
-          Subscribe
-        </Button>
-      </form>
-    </section>
-  );
+	const [email, setEmail] = useState("");
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		const emailContent = {
+			sender: {
+				name: "OutDoor Ginger Team",
+				email: "newsletter@outdoorginger.com",
+			},
+			to: [{ email: email }],
+			subject: "Welcome to Campfire Chronicles!",
+			htmlContent:
+				"<html><head></head><body><p>Welcome</p>Thank you for subcribing to Campfire Chronicles, the OutDoor Ginger Newsletter!</p></body></html>",
+		};
+		sendMail(emailContent);
+	};
+
+	return (
+		<section className="h-fit bg-ogPrimary flex flex-col items-center gap-12 text-center px-4 py-16 md:py-24">
+			<div className=" space-y-4">
+				<h1 className="font-display font-bold uppercase  text-4xl md:text-7xl text-ogPrimary-lightest m-0">
+					{title}
+				</h1>
+				<h2 className=" font-display text-ogPrimary-lighter text-2xl font-medium m-0">
+					{subtitle}
+				</h2>
+			</div>
+			<p className="text-ogPrimary-lightest max-w-lg text-base md:text-lg leading-relaxed">
+				Subscribe to get interesting stories, field reports, and other goodies
+				straight to your inbox.
+			</p>
+			<form
+				action=""
+				className="flex flex-col md:flex-row gap-4 items-center w-full max-w-xs md:max-w-md"
+			>
+				<Input
+					type="email"
+					placeholder="Your email address"
+					className="bg-ogPrimary placeholder:text-ogPrimary-lightest text-ogPrimary-lightest border-ogPrimary-lighter focus-visible:ring-0 focus-visible:ring-offset-0"
+					required
+					onChange={(e) => setEmail(e.target.value)}
+				/>
+				<Button
+					size="lg"
+					className="bg-ogPrimary-dark text-ogBG-base hover:bg-ogPrimary-darker"
+					onClick={handleSubmit}
+				>
+					Subscribe
+				</Button>
+			</form>
+		</section>
+	);
 }
 
 export default NewsletterBanner;
