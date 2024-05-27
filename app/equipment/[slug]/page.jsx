468494fac653c428e0cd8cef0c4a2be11f53aca4
Oliver G.A. <hi@oliverga.com>
2024-05-27 11:13:12 +0200

    Update margin top value on equipment page

--- app/equipment/[slug]/page.jsx
+++ app/equipment/[slug]/page.jsx
@@ -39,7 +39,7 @@ export default async function Page({ params }) {
     <main className="min-h-screen">
       <Hero imageSrc={image} height="h-48 md:h-[30vh]" bg="bg-equipment" />
       <section className="container mx-auto max-w-5xl md:grid grid-cols-2 prose pb-24">
-        <div className="md:px-8 pb-8 -mt-10 md:mt-0">
+        <div className="md:px-8 pb-8 -mt-6 md:mt-0">
           <Image
             src={image}
             alt={product.title}
