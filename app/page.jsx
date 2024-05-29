4fe7dab308e5e8b8f86f90e02c8630e763b3dfd4
Oliver G.A. <hi@oliverga.com>
2024-05-29 21:38:36 +0200

    Update homepage tagline to include "wilderness" enthusiasts

--- app/page.jsx
+++ app/page.jsx
@@ -22,7 +22,7 @@ export default function Home() {
       >
         <div id="left" className="flex flex-col w-full gap-4 mt-4 text-lg">
           <p className="text-ogBG-base max-w-[90%] opacity-80 leading-normal text-base md:text-lg ">
-            The home of adventurers, explorers, and outdoor enthusiasts
+            The home of adventurers, explorers, and wilderness enthusiasts
           </p>
           <Link href="/membership">
             <Button size="md" variant="primary" className="">
